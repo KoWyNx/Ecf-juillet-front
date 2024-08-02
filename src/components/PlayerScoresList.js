365f2671b2f97cb1c6a0a0b03042fd80a5ea9b2e
fares berrani <fberrani@hotmail.fr>
2024-08-02 17:14:49 +0200

    fix fetch player

--- src/components/PlayerScoresList.js
+++ src/components/PlayerScoresList.js
@@ -1,6 +1,5 @@
-// src/components/PlayerScoresList.js
 import React, { useState, useEffect } from 'react';
-import { getPlayerScores } from '../api';
+import { getPlayerScores } from '../api'; 
 
 const PlayerScoresList = () => {
     const [playerScores, setPlayerScores] = useState([]);
@@ -10,34 +9,50 @@ const PlayerScoresList = () => {
         const fetchPlayerScores = async () => {
             try {
                 const response = await getPlayerScores();
-                setPlayerScores(response.data);
+                const data = response.data; 
+
+                console.log('Player scores fetched:', data);
+
+                // Adapter la réponse selon la structure de données
+                setPlayerScores(data.$values || []);
             } catch (error) {
                 console.error('Error fetching player scores:', error);
-                setError('Error fetching player scores');
+                setError('Failed to fetch player scores');
             }
         };
 
         fetchPlayerScores();
     }, []);
 
+    if (error) {
+        return <div>{error}</div>;
+    }
+
     return (
         <div>
             <h1>Liste des Joueurs et Scores</h1>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
             <table>
                 <thead>
                     <tr>
                         <th>Nom du Joueur</th>
                         <th>Score</th>
+                        <th>Date</th> 
                     </tr>
                 </thead>
                 <tbody>
-                    {playerScores.map((player, index) => (
-                        <tr key={index}>
-                            <td>{player.playerName}</td>
-                            <td>{player.score}</td>
+                    {playerScores.length > 0 ? (
+                        playerScores.map((player, index) => (
+                            <tr key={index}>
+                                <td>{player.playerName}</td>
+                                <td>{player.score}</td>
+                                <td>{new Date(player.dateRecorded).toLocaleDateString()}</td> 
+                            </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan="3">Pas de joueur</td> 
                         </tr>
-                    ))}
+                    )}
                 </tbody>
             </table>
         </div>
